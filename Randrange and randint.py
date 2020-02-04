ed4af6bac3badfd6259d803a2fead3dc93204f58
Sharar Muhtasim <60645992+sharar-muhtasim@users.noreply.github.com>
2020-02-04 21:49:41 +0600

    Update Randrange and randint.py

--- Randrange and randint.py
+++ Randrange and randint.py
@@ -1,5 +1,6 @@
+#Generates a random range and then generates random numbers based on the range
 import random
 z = random.randrange(5)
 print('The range is ' + str(z))
-for ink in range(z):
+for i in range(z):
     print(random.randint(1,10))
